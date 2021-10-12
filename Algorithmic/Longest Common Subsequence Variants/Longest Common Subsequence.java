4efe70a52cb4060b4d67236293f03e0413101b3a
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2021-10-13 02:29:25 +0530

    Update Longest Common Subsequence.java

--- Algorithmic/Longest Common Subsequence Variants/Longest Common Subsequence.java
+++ Algorithmic/Longest Common Subsequence Variants/Longest Common Subsequence.java
@@ -10,25 +10,24 @@
 //  TOP DOWN APPROACH (RECURSION WITH Memoization)
 
 class Solution {
-    
+    static int t[][]=new int [1000][1000];
     public int longestCommonSubsequence(String text1, String text2) {
         int n=text1.length(),m=text2.length();
-        int t[][]=new int [n+1][m+1];
         for(int i=0;i<n+1;i++){
             for(int j=0;j<m+1;j++){
                 t[i][j]=-1;
             }
         }
-        return solve(text1,text2,n,m,t);
+        return solve(text1,text2,n,m);
     }
   
-    public int solve(String x, String y, int n, int m,int t[][]) {
+    public int solve(String x, String y, int n, int m) {
                 if(n==0 || m==0) return 0;
                 if(t[n][m]!=-1) return t[n][m];
                 if(x.charAt(n-1)==y.charAt(m-1))
-                    return t[n][m]= (1 + solve(x,y,n-1,m-1,t));
+                    return t[n][m]= (1 + solve(x,y,n-1,m-1));
                 else
-                    return t[n][m]= Math.max(solve(x,y,n,m-1,t),solve(x,y,n-1,m,t));
+                    return t[n][m]= Math.max(solve(x,y,n,m-1,t),solve(x,y,n-1,m));
             }
         }
 
