fcc90bed88f24ed1187feb9dfd35c0d0a5e3c789
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2021-10-14 05:52:40 +0530

    Update 4. Shortest Common Supersequence.java

--- Algorithmic/Longest Common Subsequence Variants/4. Shortest Common Supersequence.java
+++ Algorithmic/Longest Common Subsequence Variants/4. Shortest Common Supersequence.java
@@ -27,7 +27,7 @@ class Solution
          int t[][]=new int [m+1][n+1];
      
          for(int i=0;i<m+1;i++) t[i][0]=0;
-         for(int j=0;i<n+1;j++) t[0][j]=0;
+         for(int j=0;j<n+1;j++) t[0][j]=0;
          
          for(int i=1;i<m+1;i++){
              for(int j=1;j<n+1;j++){
