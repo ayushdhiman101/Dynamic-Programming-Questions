a8cf6eab2a56cf3f23e0728882284df7272a11b1
Ayush Dhiman <61506157+ayushdhiman101@users.noreply.github.com>
2021-10-14 17:34:29 +0530

    Update 7.Minimum number of deletions to make a string palindromic.java

--- Algorithmic/Longest Common Subsequence Variants/7.Minimum number of deletions to make a string palindromic.java
+++ Algorithmic/Longest Common Subsequence Variants/7.Minimum number of deletions to make a string palindromic.java
@@ -33,7 +33,7 @@ class Solution
         for(int j=1;j<n+1;j++) t[0][j]=0;
         
          for(int i=1;i<m+1;i++){
-              for(int j=1;j<n+1;n++){
+              for(int j=1;j<n+1;j++){
                     if(a.charAt(i-1)==b.charAt(j-1))
                         t[i][j]=1+t[i-1][j-1];
                     else 
